545ed837a347b7f39662b416ed4d9295d3a5d5d6
Steve Winton <stevewinton@gmail.com>
2021-08-23 15:29:47 -0500

    Add package skeleton

---
+++ cleanup.js
@@ -0,0 +1,8 @@
+// Revoke token
+// https://docs.github.com/en/rest/reference/apps#delete-an-app-token
+
+const core = require('@actions/core');
+
+(async () => {
+  core.info(`Revoking a token...`);
+})();
---
+++ index.js
@@ -0,0 +1,8 @@
+// Create token
+// https://docs.github.com/en/rest/reference/apps#create-a-scoped-access-token
+
+const core = require('@actions/core');
+
+(async () => {
+  core.info(`Creating a token...`);
+})();
